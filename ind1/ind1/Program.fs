43eb164b1ffe6690d4f5159322ecb28af145c35d
Nadezhda <pinkamenapie2907@gmail.com>
2020-04-30 19:54:49 +0300

    индивидуальное задание1F
    произведение

---
+++ ind1/ind1/Program.fs
@@ -0,0 +1,25 @@
+﻿
+let poslch x y =
+let pr x y =
+    if x !=y then x*y
+
+
+let ir_1 = 
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    let c = System.Convert.ToInt32(System.Console.ReadLine())
+    poslch x y  
+
+
+
+
+
+
+[<EntryPoint>]
+let main argv = 
+    
+    
+    let Ans = ir_1
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey()
+    
+    0 

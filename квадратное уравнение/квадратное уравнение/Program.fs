9a39aa5d03ecdb6722bf141f55224812b1fea0f2
Nadezhda <pinkamenapie2907@gmail.com>
2020-05-13 20:14:38 +0300

    квадратное уравнение
    создание forms

---
+++ квадратное уравнение/квадратное уравнение/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open System.Windows.Forms
+
+let form = new Form()
+form.Visible <- true
+form.Text <- "F# Forms"
+
+let label = new Label()
+label.Text <- "F# Labels"
+
+form.Controls.Add(label)
+
+[<STAThread>]
+Application.Run(form)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code

f4335305438b2a6d5be7724c32b6ba67f1d5f09b
Nadezhda <pinkamenapie2907@gmail.com>
2020-05-31 23:26:48 +0300

    ку
    победа

--- квадратное уравнение/квадратное уравнение/Program.fs
+++ квадратное уравнение/квадратное уравнение/Program.fs
@@ -15,58 +15,28 @@ let textBox1 = new TextBox (Top = 60, Left = 40, Width = 70)
 let textBox2 = new TextBox (Top = 117, Left = 40, Width = 70) 
 let textBox3 = new TextBox (Top = 174, Left = 40, Width = 70)
 let ThisButton = new Button(Top = 330 ,Left = 135, Width = 75, Height = 23, Text = "посчитать" )
-let richtextbox = new RichTextBox (Top = 32, Left = 180, Width = 150, Height = 200)
 
  
 
 MainForm.Controls.Add(Label1)
 MainForm.Controls.Add(Label2)
 MainForm.Controls.Add(Label3)
+
 MainForm.Controls.Add(textBox1)
 MainForm.Controls.Add(textBox2)
 MainForm.Controls.Add(textBox3)
+
 MainForm.Controls.Add(ThisButton)
-//MainForm.Controls.Add(richtextbox)
-
-
-//let discrim =  Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text) 
-
-
-
-//let pr1  = 2*2
-//let pr _ = MessageBox.Show (Convert.ToString(2*2),"умножение")|>ignore
-
-//let _ = ThisButton.Click.Add(pr)
-
 
 
 let pr _ = 
      if ( Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text) ) > 0.0 then
         MessageBox.Show( ("X1 = " + Convert.ToString( - ( ( ( (Convert.ToDouble(textBox2.Text ) ) - sqrt (Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text)) ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) ) ) ) ) + "\nX2 = " + Convert.ToString( - ( ( ( (Convert.ToDouble(textBox2.Text ) ) + sqrt (Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text)) ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) ) ) ) ) ) , "умножение") |>ignore
- 
+     else if ( Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text) ) = 0.0 then
+           MessageBox.Show( ("X = " + Convert.ToString( - ( ( ( (Convert.ToDouble(textBox2.Text ) ) ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) ) ) ) ) ) , "умножение") |>ignore
+           else MessageBox.Show("НЕТ КОРНЕЙ!", "умножение") |>ignore
 
-
-
-
-
-
-//let pr1 =  
- //   if discrim > 0 then 
-//let pr _ = MessageBox.Show ( - ( ( ( (Convert.ToDouble(textBox2.Text ) ) + sqrt discrim ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) ) ) ) |> ignore
-//let _ = ThisButton.Click.Add(pr) 
-
-
-//let  x2: double = 
- //        if discrim > 0 then let x2 _ = MessageBox.Show (- ( (Convert.ToDouble(textBox2.Text ) ) ) - sqrt discrim ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) )|> ignore
-
-//let discrim = 0 then  let x1 _ = MessageBox.Show (-((System.Convert.ToInt32 (Label2.Text) / (2 * ( (System.Convert.ToInt32(Label1.Text) ) ) ) ) ) ) |> ignore 
-
-//elif discrim < 0 then 
-  //               let x1 _ = MessageBox.Show(Text = "действительных корней не существует")
-
-//let _= ThisButton.Click.Add ( x1)  
-
-    
+let _ = ThisButton.Click.Add(pr)
 
 [<EntryPoint>]
     let main argv = 

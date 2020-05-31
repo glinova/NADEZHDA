9468042388ee0c172d0c0e2c9633d3ce9b829162
Nadezhda <pinkamenapie2907@gmail.com>
2020-05-31 15:54:12 +0300

    ку
    зря я раньше не комментила, теперь сложно с умножением, тк было много идей, а теперь, вспоминай, что было, а что не было (хотя, кажется, я снова вернулась в начало, блинб)

--- квадратное уравнение/квадратное уравнение/Program.fs
+++ квадратное уравнение/квадратное уравнение/Program.fs
@@ -7,39 +7,48 @@ open System.Drawing
 let MainForm = new Form(Width = 379, Height = 470, Text = "квадратное уравнение Form" )
 let Label1 = new Label(Top = 32, Left = 40, Text = "a")
 let Label2 = new Label(Left = 40, Top = 90, Text = "b")
-let Label3 = new Label(Left = 40, Top = 148, Text = "с")
-let textBox3 = new TextBox (Top = 174, Left = 40, Width = 70)
+//let Label3 = new Label(Left = 40, Top = 148, Text = "с")
+//let textBox3 = new TextBox (Top = 174, Left = 40, Width = 70)
 let textBox1 = new TextBox (Top = 60, Left = 40, Width = 70) 
 let textBox2 = new TextBox (Top = 117, Left = 40, Width = 70) 
 let ThisButton = new Button(Top = 330 ,Left = 135, Width = 75, Height = 23, Text = "посчитать" )
-let richTextBox = new RichTextBox (Top = 32, Left = 180, Width = 150, Height = 200)
+//let richtextbox = new RichTextBox (Top = 32, Left = 180, Width = 150, Height = 200)
 
  
 
-
 MainForm.Controls.Add(Label1)
 MainForm.Controls.Add(Label2)
-MainForm.Controls.Add(Label3)
+//MainForm.Controls.Add(Label3)
 MainForm.Controls.Add(textBox1)
 MainForm.Controls.Add(textBox2)
-MainForm.Controls.Add(textBox3)
+//MainForm.Controls.Add(textBox3)
 MainForm.Controls.Add(ThisButton)
-MainForm.Controls.Add(richTextBox)
+//MainForm.Controls.Add(richtextbox)
 
 
-let discrim = ( Convert.ToDouble(texBox2.Text) * Convert.ToDouble(texBox2.Text) ) - ( 4 * Convert.ToDouble(texBox1.Text) * Convert.ToDouble(texBox3.Text) )
+//let discrim =  Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)  -  4.0 * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox3.Text) 
 
-let f (x1: double , x2: double) = 
-    if discrim > 0 
-    then let x1 _ = MessageBox.Show ( - ( ( ( (Convert.ToDouble(texBox2.Text ) ) + sqrt discrim ) / (2 * (Convert.ToDouble(texBox1.Text) ) ) ) )|> ignore
-         let x2 _ = MessageBox.Show (- ( (System.Convert.ToInt32 (Label2.Text) ) ) - sqrt discrim ) / (2 * (System.Convert.ToInt32(Label1.Text) ) )|> ignore
+let pr x y = x*y
+let proisved =
+    let a = Convert.ToDouble(textBox1.Text) 
+    let b = Convert.ToDouble(textBox2.Text)
+    (pr a b)
+
+
+
+//let x1:double =
+ //   if discrim > 0 then 
+ //   let x1 _ = MessageBox.Show ( - ( ( ( (Convert.ToDouble(textBox2.Text ) ) + sqrt discrim ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) ) ) ) |> ignore
+
+//let  x2: double = 
+ //        if discrim > 0 then let x2 _ = MessageBox.Show (- ( (Convert.ToDouble(textBox2.Text ) ) ) - sqrt discrim ) / (2.0 * (Convert.ToDouble(textBox1.Text) ) )|> ignore
 
 //let discrim = 0 then  let x1 _ = MessageBox.Show (-((System.Convert.ToInt32 (Label2.Text) / (2 * ( (System.Convert.ToInt32(Label1.Text) ) ) ) ) ) ) |> ignore 
 
 //elif discrim < 0 then 
   //               let x1 _ = MessageBox.Show(Text = "действительных корней не существует")
 
-//let _= ThisButton.Click.Add (discrim)  
+//let _= ThisButton.Click.Add ( x1)  
 
     
 

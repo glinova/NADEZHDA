159dd3c85d7ff38f190b4c0e7f5b6d0dda611f36
Nadezhda <pinkamenapie2907@gmail.com>
2020-06-07 13:43:19 +0300

    из12
    это будет долго

---
+++ из12/из12/Program.fs
@@ -0,0 +1,22 @@
+﻿let rec read_list n = 
+    if n=0 then []
+    else
+    let head = System.Convert.ToInt32(System.Console.ReadLine())
+    let tail = read_list (n-1)
+    head::tail
+
+let read_data=
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    read_list n
+
+    
+let rec ref
+
+    
+    
+    
+    
+let prog list =
+    match list with
+    | []-> 0
+    | head::tail -> prog2 prov head tail 

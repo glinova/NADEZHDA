d2c827a618aa27ced4fd55c82f2bd11426a72c96
Nadezhda <pinkamenapie2907@gmail.com>
2020-06-08 23:27:03 +0300

    из12
    правильная перестановка

--- из12/из12/Program.fs
+++ из12/из12/Program.fs
@@ -9,19 +9,13 @@ let read_data=
     let n = System.Convert.ToInt32(System.Console.ReadLine())
     read_list n
 
-let findcount list el =
-    let rec count list el =
-        match list with 
-    |   [] -> false
-    |   h::t -> if list element then 
-                            if elem=h then count (n+1) el t
-                            else count n el t
-count 0 elem count
-
-
-
-
-
+let findcount list elem =
+    let rec find list elem count = function
+    |[] -> count 
+    |h::t -> if elem = h then find (count+1) elem t
+                        else find elem t
+ find 0 elem list
+        
 
 
 
@@ -31,23 +25,18 @@ let podshet list el =
     |h::t -> if 
 
 
-
+let rec findReapeats list elem = function
+    |[] -> true
+    |h::t -> if h = elem  then false
+             else findReapeats t elem t
 
 
 let rec uniq L1 list elem = function
     |[] -> L1
     | h::t -> let l = h::L1 if (findReapeats list h) then
-                            uniq l list h t
-                            else uniq L1 list elem t
+                                uniq l list h t
+                                else uniq L1 list elem t
 
-
-
-
-
-let rec findReapeats list elem = function
-    |[] -> true
-    |h::t -> if h = elem  then false
-             else findReapeats t elem t
 
 
 

e7fea379b88c9a9909f3441e6cf0b53d4018d96c
Nadezhda <pinkamenapie2907@gmail.com>
2020-06-08 13:54:21 +0300

    из12
    я немного начинаю понимать

--- из12/из12/Program.fs
+++ из12/из12/Program.fs
@@ -9,12 +9,31 @@ let read_data=
     let n = System.Convert.ToInt32(System.Console.ReadLine())
     read_list n
 
-    
-let rec list =
-    match list with
-    | [] -> 0
-    | head::tail -> 
 
+let unic list el =
+    let rec 
+
+
+
+let
+
+
+let rec in_list_exclude list el = function
+    [] ->true
+    |h::t -> if h=el then false
+                else in_list_exclude t el t
+
+
+
+
+    
+let rec count list el =
+    match list with 
+    | [] -> false
+    | head::tail -> if n then 
+                         if el=h then count (n+1) el t
+                         else count n el t
+count 0 el list
     
     
     

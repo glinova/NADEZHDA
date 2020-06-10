fed793ac5503fcb676c1ce0962b3ae87696b3596
Nadezhda <pinkamenapie2907@gmail.com>
2020-06-10 11:02:15 +0300

    из4
    исправление ошибок

--- из4.pl
+++ из4.pl
@@ -1,41 +1,41 @@
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
-iz4:-Deti[_,_,_,_,_],
-    in_list(Deti,[Alik,_]),
-    in_list(Deti,[Boris,_]),
-    in_list(Deti,[Vitya,_]),
-    in_list(Deti,[Lena,_]),
-    in_list(Deti,[Dasha,_]),
-    in_list(Deti,[_,Harkov]),
-    in_list(Deti,[_,Ymani]),
-    in_list(Deti,[_,Poltava]),
-    in_list(Deti,[_,Slavyansk]),
-    in_list(Deti,[_,Kramatorsk]),
+iz4:-Deti=[_,_,_,_,_],
+    in_list(Deti,[alik,_]),
+    in_list(Deti,[borya,_]),
+    in_list(Deti,[vitya,_]),
+    in_list(Deti,[lena,_]),
+    in_list(Deti,[dasha,_]),
+    in_list(Deti,[_,harkov]),
+    in_list(Deti,[_,ymani]),
+    in_list(Deti,[_,poltava]),
+    in_list(Deti,[_,slavyansk]),
+    in_list(Deti,[_,kramatorsk]),
 
     (
-     ( in_list(Deti,[Alik,Ymani]);
-       in_list(Deti,[Borya,Kramatorsk])),
+        ( in_list(Deti,[alik,ymani]);
+          in_list(Deti,[borya,kramatorsk])),
 
-       ( in_list(Deti,[Borya,Harkov]);
-         in_list(Deti,[Vitya,Harkov])),
+       ( in_list(Deti,[borya,harkov]);
+         in_list(Deti,[vitya,harkov])),
 
-        ( in_list(Deti,[Vitya,Slavyansk]);
-        in_list(Deti,[Lena,Harkov])),
+        ( in_list(Deti,[vitya,slavyansk]);
+          in_list(Deti,[lena,sarkov])),
 
-        (   in_list(Deti,[Dasha,Ymani]);
-        in_list(Deti,[Lena,Kramatorsk])),
-    )
+        (   in_list(Deti,[dasha,ymani]);
+            in_list(Deti,[lena,kramatorsk]))
+    ),
 
-    in_list(Deti,[A,Harkov]),
-    in_list(Deti,[B,Ymani]),
-    in_list(Deti,[C,Poltava]),
-    in_list(Deti,[D,Slavyansk]),
-    in_list(Deti,[E,Kramatorsk]),
-    write("Harkov"),write(" "),write(A),
-    write("Ymani"), write(" "),write(B),
-    write("Poltava"), write(" "),write(C),
-    write("Slavyansk"),write(" "),write(D),
+    in_list(Deti,[A,harkov]),
+    in_list(Deti,[B,ymani]),
+    in_list(Deti,[C,poltava]),
+    in_list(Deti,[D,slavyansk]),
+    in_list(Deti,[E,kramatorsk]),
+    write("Harkov"),write(" "),write(A),nl,
+    write("Ymani"), write(" "),write(B),nl,
+    write("Poltava"), write(" "),write(C),nl,
+    write("Slavyansk"),write(" "),write(D),nl,
     write("Kramatorsk"),write(" "),write(E),!.
 
 
